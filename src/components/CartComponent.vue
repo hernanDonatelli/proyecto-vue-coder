14467cfecead1cc5bc984b5fa6a36d3f13d5f550
Hernan Donatelli <hernandonatelli@gmail.com>
2022-12-12 12:09:47 -0300

    agregado de componente Fila, modificaciones sobre esto

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -15,13 +15,17 @@
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td colspan="5">
-                  <p class="cartText text-center w-100">
-                    No hay productos ingresados aún.
-                  </p>
-                </td>
-              </tr>
+              <fila-component
+              is="fila-component"
+              v-for="(producto, index) in cart"
+              :key="index"
+              :productos="productos"
+              :id="producto.id"
+              :nombre="producto.nombre"
+              :marca="producto.marca"
+              :precio="producto.precio"
+              :info="producto.categoria"
+              />
             </tbody>
           </table>
         </div>
@@ -33,8 +37,56 @@
 </template>
 
 <script>
+import FilaComponent from './FilaComponent.vue';
+
 export default {
   name: "CartComponent",
+  props: {
+    productos: Array
+  },
+  components: {
+    FilaComponent
+  },
+  data() {
+    return {
+      cart: [
+      {
+          id: 1,
+          marca: "Inti Zen",
+          nombre: "Ilumine",
+          img: "https://i.imgur.com/V0MO17C.png",
+          descripcion:
+            "Breakfast Tea que combina un té negro de Ceylon cosechado en altura y aire puro, con notas bien definidas de un té negro de Assam que le dan cuerpo, sabor y aroma a la taza.",
+          precio: 254.45,
+          stock: true,
+          categoria: "Infusiones",
+        },
+        {
+          id: 11,
+          marca: "Chocolate Colonial",
+          nombre: "Organico 70% Cacao",
+          img: "https://i.imgur.com/0wSVdya.png",
+          descripcion:
+            "Caja que contiene 10 unidades de 100 gramos. Peso neto 1 kg. Ingredientes: licor de cacao orgánico, azúcar orgánico, manteca de cacao orgánica.",
+          precio: 547.19,
+          stock: true,
+          categoria: "Chocolates",
+        },
+        {
+          id: 15,
+          marca: "Cocoon",
+          nombre: "Sin Azucar 1l",
+          img: "https://i.imgur.com/c7aRuve.png",
+          descripcion:
+            "Bebida de almendras con verdadero sabor a almendras. Sin azúcar, sin saborizantes, sin aditivos artificiales. ¡Naturalmente rica!",
+          precio: 255.78,
+          stock: true,
+          categoria: "Leches de Almendras",
+        }
+      ]
+    }
+  },
+
 };
 </script>
 
@@ -45,8 +97,9 @@ export default {
 }
 .cartTable th{
   font-family: Oswald;
+  font-size: 1.2rem;
   text-transform: uppercase;
-  font-weight: 400;
+  font-weight: 500;
 }
 .cartText {
   font-size: 0.85rem;
---
+++ src/components/FilaComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <tr>
+      <td>{{ id }}</td>
+      <td>{{ nombre }}</td>
+      <td>{{ marca }}</td>
+      <td>${{ precio }}</td>
+      <td>{{ info }}</td>
+    </tr>
+</template>
+
+<script>
+export default {
+  name: "FilaComponente",
+  props: {
+    id: Number,
+    nombre: String,
+    marca: String,
+    precio: Number,
+    info: String,
+  },
+};
+</script>
+
+<style scoped>
+</style>
